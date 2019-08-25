3067718eaf19d0c77f03a226354dff01f75b7d70
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-25 17:12:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,6 +9,9 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @total += (price * quantity)
+    quantity.times do
+      @cart << title
+    end
   end
 
   def apply_discount
